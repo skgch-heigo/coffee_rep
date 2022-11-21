68af052fdde83178b98745a987f0d5225519a56f
skgch-heigo <mstal64@yandex.ru>
2022-11-21 22:25:22 +0400

    version 1.0 added everything

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+import sys
+import sqlite3
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from PyQt5 import uic
+from PyQt5.QtSql import QSqlDatabase, QSqlTableModel, QSqlRelationalTableModel, QSqlRelation, QSqlQuery
+from PyQt5.QtWidgets import QApplication, QInputDialog, QDialog, QStatusBar, QTableWidget
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+from PyQt5.QtGui import QIcon
+from PyQt5 import QtCore
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.con = sqlite3.connect("coffee.db")
+        self.cur = self.con.cursor()
+        res = self.con.cursor().execute("""SELECT * FROM coffee""").fetchall()
+        # Заполним размеры таблицы
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setHorizontalHeaderLabels(["ID", "name", "roast", "type", "flavour", "cost", "volume"])
+        # Заполняем таблицу элементами
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+
+    def closeEvent(self, event):
+        self.con.close()
 
 
-# Press the green button in the gutter to run the script.
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
